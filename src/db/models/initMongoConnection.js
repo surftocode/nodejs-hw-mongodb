a07412076430b98f82079aba3b3ff41ba32c3e7f
Merve Delibaşoglu <mervedelibasoglu.dev@gmail.com>
2025-08-20 09:47:51 +0300

    link düzenlendi

--- src/db/models/initMongoConnection.js
+++ src/db/models/initMongoConnection.js
@@ -4,9 +4,11 @@ import mongoose from "mongoose";
 
 export const initMongoConnection = async () => {
   try {
-    const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster0.p7rntty.mongodb.net/?retryWrites=true&w=majority`;
-
-    await mongoose.connect(uri);
+    const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_URL}/?retryWrites=true&w=majority&appName=Cluster0`;
+    console.log(uri);
+    await mongoose.connect(uri, {
+      dbName: process.env.MONGODB_DB_NAME,
+    });
     console.log("Mongo connection successfully established!");
   } catch (err) {
     console.error("Error:", err.message);
