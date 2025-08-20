bb742154195efd4c43d5f0e8c7ab66d44e465e21
Merve Delibaşoglu <mervedelibasoglu.dev@gmail.com>
2025-08-20 08:43:49 +0300

    connectin rl düzeltildi

--- src/db/models/initMongoConnection.js
+++ src/db/models/initMongoConnection.js
@@ -4,9 +4,7 @@ import mongoose from "mongoose";
 
 export const initMongoConnection = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URL,{
-      dbName: process.env.MONGODB_DB
-    });
+    await mongoose.connect(process.env.MONGODB_URL);
     console.log("Mongo connection successfully established!");
   } catch (err) {
     console.error("Error:", err.message);
