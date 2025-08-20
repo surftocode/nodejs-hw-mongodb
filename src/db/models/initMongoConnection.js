afc3ab9a436667a90240b975625e5ca327c308f8
Merve Delibaşoglu <mervedelibasoglu.dev@gmail.com>
2025-08-20 09:30:49 +0300

    mongodb url düzenlendi.

--- src/db/models/initMongoConnection.js
+++ src/db/models/initMongoConnection.js
@@ -1,10 +1,12 @@
 import dotenv from "dotenv";
-dotenv.config({debug:false});
+dotenv.config({ debug: false });
 import mongoose from "mongoose";
 
 export const initMongoConnection = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URL);
+    const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster0.p7rntty.mongodb.net/?retryWrites=true&w=majority`;
+
+    await mongoose.connect(uri);
     console.log("Mongo connection successfully established!");
   } catch (err) {
     console.error("Error:", err.message);
