26b6ccd08fdd1cc9ac03df5db405d2aa37b1b706
Merve Delibaşoglu <mervedelibasoglu.dev@gmail.com>
2025-08-27 16:40:46 +0300

    route düzenlendi

--- src/routers/contactRouter.js
+++ src/routers/contactRouter.js
@@ -13,23 +13,7 @@ import cors from "cors";
 import { ctrlWrapper } from "../utils/ctrlWrapper.js";
 import pino from "pino-http";
 dotenv.config();
-const app = express();
-app.use(
-  express.json({
-    type: ["application/json", "application/vnd.api+json"],
-  })
-);
-app.use(cors());
-app.use(
-  pino({
-    transport: {
-      target: "pino-pretty",
-    },
-  })
-);
 const router = Router();
-app.use("/", router);
-
 router.get("/", (req, res) => {
   res.send("server is working");
 });
@@ -40,7 +24,6 @@ router.get("/contacts/:id", ctrlWrapper(getContactsById));
 router.post("/contacts", ctrlWrapper(createContact));
 router.patch("/contacts/:id", ctrlWrapper(updatedContactController));
 router.delete("/contacts/:id", ctrlWrapper(deleteContactController));
-router.use(notFoundHandler);
-router.use(errorHandler);
+
 
 export { router };
--- src/server.js
+++ src/server.js
@@ -4,9 +4,15 @@ import pino from "pino-http";
 import cors from "cors";
 import { initMongoConnection } from "./db/models/initMongoConnection.js";
 import { router } from "./routers/contactRouter.js";
+import { errorHandler } from "./middlewares/errorHandler.js";
+import { notFoundHandler } from "./middlewares/notFoundHandler.js";
 dotenv.config();
 const app = express();
-app.use(express.json());
+app.use(
+  express.json({
+    type: ["application/json", "application/vnd.api+json"],
+  })
+);
 const logger = pino({
   transport: {
     target: "pino-pretty",
@@ -23,6 +29,8 @@ app.use(cors());
 export const setupServer = async () => {
   await initMongoConnection();
   app.use("/", router);
+  app.use(notFoundHandler);
+  app.use(errorHandler);
 
   app.listen(process.env.PORT || 3000, () => {
     console.log(`Server is running on port ${process.env.PORT || 3000}`);
