86a7b2c033903d721d46e98f1b1c03a0884047a1
Merve Delibaşoglu <mervedelibasoglu.dev@gmail.com>
2025-08-31 11:53:33 +0300

    server kodları dğzenlendi

--- src/routers/contactRouter.js
+++ src/routers/contactRouter.js
@@ -1,7 +1,6 @@
 import express, { Router } from "express";
 import dotenv from "dotenv";
-import { notFoundHandler } from "../middlewares/notFoundHandler.js";
-import { errorHandler } from "../middlewares/errorHandler.js";
+
 import {
   createContact,
   deleteContactController,
@@ -9,21 +8,17 @@ import {
   getContactsById,
   updatedContactController,
 } from "../controllers/contactController.js";
-import cors from "cors";
 import { ctrlWrapper } from "../utils/ctrlWrapper.js";
-import pino from "pino-http";
 dotenv.config();
-const router = Router();
+export const router = Router();
 router.get("/", (req, res) => {
   res.send("server is working");
 });
-
+router.get("/test-router",(req,res)=>{res.send("router çalıştı")});
 router.get("/contacts", ctrlWrapper(getAllContacts));
-
 router.get("/contacts/:id", ctrlWrapper(getContactsById));
 router.post("/contacts", ctrlWrapper(createContact));
 router.patch("/contacts/:id", ctrlWrapper(updatedContactController));
 router.delete("/contacts/:id", ctrlWrapper(deleteContactController));
 
-
-export { router };
+// export { router };
--- src/server.js
+++ src/server.js
@@ -2,8 +2,8 @@ import dotenv from "dotenv";
 import express from "express";
 import pino from "pino-http";
 import cors from "cors";
-import { initMongoConnection } from "./db/models/initMongoConnection.js";
 import { router } from "./routers/contactRouter.js";
+import { initMongoConnection } from "./db/models/initMongoConnection.js";
 import { errorHandler } from "./middlewares/errorHandler.js";
 import { notFoundHandler } from "./middlewares/notFoundHandler.js";
 dotenv.config();
@@ -26,12 +26,20 @@ const logger = pino({
 });
 app.use(logger);
 app.use(router);
+app.use((req, res, next) => {
+  console.log("after router", req.method, req.path);
+  next();
+});
+app.use(notFoundHandler);
+app.use(errorHandler);
 
 export const setupServer = async () => {
   await initMongoConnection();
 
-  app.use(notFoundHandler);
-  app.use(errorHandler);
+  app.use((req, res) => {
+    console.log("Hiçbir route eşleşmedi", req.method, req.path);
+    res.status(404).send("hiçbr route eşleşmedi");
+  });
 
   app.listen(process.env.PORT || 3000, () => {
     console.log(`Server is running on port ${process.env.PORT || 3000}`);
