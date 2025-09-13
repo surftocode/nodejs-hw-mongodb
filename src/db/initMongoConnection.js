5f3bb731e2434db783ae464a7df219b4efd79e73
Merve Delibaşoglu <mervedelibasoglu.dev@gmail.com>
2025-09-13 18:17:20 +0300

    MongoDB bağlantısı düzenlendi.

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -1,12 +1,12 @@
 import dotenv from "dotenv";
-dotenv.config();
 import mongoose from "mongoose";
-
+dotenv.config();
 export const initMongoConnection = async () => {
   try {
-    const URI = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_URL}/${process.env.MONGODB_DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`;
-    await mongoose.connect(URI);
+    const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster0.p7rntty.mongodb.net/${process.env.MONGODB_DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`;
+    await mongoose.connect(uri);
     console.log("Mongo connection successfully established!");
+    console.log("connection db", mongoose.connection.name);
   } catch (err) {
     console.error("Error:", err.message);
     process.exit(1);
