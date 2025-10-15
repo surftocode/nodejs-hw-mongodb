4ffecf06a4a8586532e697a8e7117321fe02c9c9
Merve Delibaşoglu <mervedelibasoglu.dev@gmail.com>
2025-10-15 12:50:16 +0300

    import hatası düzeltildi

--- src/routers/authRouter.js
+++ src/routers/authRouter.js
@@ -5,7 +5,7 @@ import {
   logoutController,
   registerController,
 } from "../controllers/auth.js";
-import { User } from "../db/models/user.js";
+import User from "../db/models/user.js";
 import { ctrlWrapper } from "../utils/ctrlWrapper.js";
 import { userSchema } from "../db/models/user.js";
 import { validateBody } from "../validation/validateUser.js";
@@ -17,15 +17,10 @@ router.get("/", (req, res) => {
   res.send("auth router is working");
 });
 
-router.post("/register", 
-validateBody(User), 
-ctrlWrapper(registerController));
+router.post("/register", validateBody(User), ctrlWrapper(registerController));
 
-router.post("/login", 
-ctrlWrapper(loginUserController));
+router.post("/login", ctrlWrapper(loginUserController));
 
-router.post("/logout",
- authenticate,
- ctrlWrapper(logoutController));
+router.post("/logout", authenticate, ctrlWrapper(logoutController));
 
 export default router;
