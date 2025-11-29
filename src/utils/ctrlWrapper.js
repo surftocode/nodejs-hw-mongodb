80cd0687afba9f324d002240d406485a86c446c9
Merve Delibaşoglu <mervedelibasoglu.dev@gmail.com>
2025-11-29 18:52:48 +0300

    ..

--- src/utils/ctrlWrapper.js
+++ src/utils/ctrlWrapper.js
@@ -3,7 +3,7 @@ export const ctrlWrapper=(controller)=>{
         try {
             await controller(req,res,next)
         } catch (err) {
-            next(err);
+            next(err.message);
             
         }
     }
