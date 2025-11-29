cb4cb03534a0028428fa6f666b0886c1adbff686
Merve Delibaşoglu <mervedelibasoglu.dev@gmail.com>
2025-11-29 20:18:37 +0300

    smtp fro düzenlendi.

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -1,23 +1,17 @@
-import nodemailer from 'nodemailer';
-import {SMTP} from '../constants/index.js';
+import nodemailer from "nodemailer";
+import { SMTP } from "../constants/index.js";
 
+const transporter = nodemailer.createTransport({
+  host: SMTP.SMTP_SERVER,
+  port: 587,
+  secure: false, // true for 465, false for other ports
+  auth: {
+    user: SMTP.SMTP_FROM,
+    pass: SMTP.SMTP_PASSWORD,
+  },
+});
 
-
-
-const transporter= nodemailer.createTransport({
-    host:SMTP.SMTP_SERVER,
-    port: 587,
-    secure: false, // true for 465, false for other ports
-    auth: {
-      user:SMTP.SMTP_LOGIN,
-      pass: SMTP.SMTP_PASSWORD,
-    },
-  });
-
-  export const sendEmail=async (options)=>{
-    const result=await transporter.sendMail(options);
-    return result;
-        
- 
-
-  }
+export const sendEmail = async (options) => {
+  const result = await transporter.sendMail(options);
+  return result;
+};
