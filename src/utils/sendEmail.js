cc831dcdba4afe1f4d0cc3ea8debc8ffcd36a6cc
Merve Delibaşoglu <mervedelibasoglu.dev@gmail.com>
2025-11-30 13:29:11 +0300

    sendemail denemesi.

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -12,7 +12,25 @@ const transporter = nodemailer.createTransport({
 });
 
 export const sendEmail = async (options) => {
-  const result = await transporter.sendMail(options);
+  try {
+    console.log("Sending email with options:", options);
+    console.log("SMTP CONFİG:", {
+      host: SMTP.SMTP_SERVER,
+      port: 587,
+      secure: false, // true for 465, false for other ports
+      auth: {
+        user: SMTP.SMTP_LOGIN,
+        pass: SMTP.SMTP_PASSWORD,
+      },
+    });
+    const result = await transporter.sendMail(options);
 
-  console.log("Message sent:", result.messageId);
+    console.log("Message sent:", result.messageId);
+  } catch (error) {
+    console.error("Error sending email:", error);
+    console.error("Error message:",error.message);
+    console.error("error code",error.code);
+    console.error("error response",error.response);
+    throw error;
+  }
 };
